9d403c31a9cc3cb32ee4a249f3ec29a800809b31
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2017-04-04 15:14:37 +0200

    Added owned vehicles functions. /myvehicles

---
+++ RoleplayServer/resources/vehicle_manager/VehicleOwnership.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTANetworkServer;
+using GTANetworkShared;
+using RoleplayServer.resources.player_manager;
+
+namespace RoleplayServer.resources.vehicle_manager
+{
+    class VehicleOwnership : Script
+    {
+        public VehicleOwnership()
+        {
+            API.onClientEventTrigger += API_onClientEventTrigger;
+        }
+
+        private void API_onClientEventTrigger(Client sender, string eventName, params object[] arguments)
+        {
+            switch (eventName)
+            {
+                case "myvehicles_locatecar":
+                    Character character = API.getEntityData(sender, "Character");
+                    vehicle_manager.Vehicle veh =
+                        character.OwnedVehicles.Single(x => x.NetHandle.Value == Convert.ToInt32(arguments[0]));
+                    Vector3 loc = API.getEntityPosition(veh.NetHandle);
+                    API.triggerClientEvent(sender, "myvehicles_setCheckpointToCar", loc.X, loc.Y, loc.Z);
+                    API.sendChatMessageToPlayer(sender, "A checkpoint has been set to the vehicle.");
+                    break;
+            }
+        }
+
+        [Command("myvehicles")]
+        public void myvehicles_cmd(Client player)
+        {
+            //Get all owned vehicles and send them.
+            Character character = API.getEntityData(player, "Character");
+            string[][] cars = character.OwnedVehicles.Select(x => new [] { API.getVehicleDisplayName(x.VehModel), x.Id.ToString(), x.NetHandle.Value.ToString()}).ToArray();
+
+            API.triggerClientEvent(player, "myvehicles_showmenu", API.toJson(cars));
+        }
+    }
+}
---
+++ RoleplayServer/resources/vehicle_manager/VehicleOwnership.js
@@ -0,0 +1,68 @@
+﻿/// <reference path="../types-gtanetwork/index.d.ts" />
+
+var menuPool;
+
+API.onServerEventTrigger.connect((eventName, args) => {
+    switch(eventName) {
+        case "myvehicles_showmenu":
+            menuPool = API.getMenuPool();
+            var myCars = API.createMenu("Your Vehicles", "Select a vehicle to manage.", 0, 0, 4);
+            var carsList = JSON.parse(args[0]);
+            for (var i = 0; i < carsList.length; i++) {
+                var car;
+                if (carsList[i][2] == "0")
+                    car = API.createMenuItem(carsList[i][0] + " - Unspawned(VIP)", `NetHandle: #${carsList[i][2]} | ID: #${carsList[i][1]}`);
+                else
+                    car = API.createMenuItem(carsList[i][0], `NetHandle: #${carsList[i][2]} | ID: #${carsList[i][1]}`);
+                myCars.AddItem(car);
+            }
+
+            menuPool.Add(myCars);
+            myCars.Visible = true;
+
+            //Setup submenu.
+            var actionsMenu = API.createMenu("Manage Vehicle", `Select an action to do your vehicle.`, 0, 0, 4);
+            actionsMenu.AddItem(API.createMenuItem("Locate", "Set a checkpoint to the location of your car."));
+            actionsMenu.AddItem(API.createMenuItem("Sell", "Sell your car."));
+            menuPool.Add(actionsMenu);
+
+            actionsMenu.OnMenuClose.connect(function (closesender) {
+                myCars.Visible = true;
+            });
+
+            var currentSelectedCar = -1;
+            myCars.OnItemSelect.connect(function (csender, citem, cindex) {
+                myCars.Visible = false;
+                actionsMenu.Visible = true;
+                currentSelectedCar = cindex;
+                API.sendChatMessage(`You are managing your ~r~${carsList[cindex][0]}~w~.`);
+            });
+
+            actionsMenu.OnItemSelect.connect(function (osender, oitem, oindex) {
+                if (currentSelectedCar !== -1) {
+                    switch (oindex) {
+                        case 0:
+                            if (carsList[currentSelectedCar][2] !== "0") {
+                                API.triggerServerEvent("myvehicles_locatecar", carsList[currentSelectedCar][2]);
+                            } else
+                                API.sendChatMessage("You can't locate an unspawned car.");
+                            break;
+                        case 1:
+                            API.sendChatMessage("Sell pressed.");
+                            break;
+                    }
+                }
+            });
+            break;
+
+        case "myvehicles_setCheckpointToCar":
+            API.setWaypoint(args[0], args[1]);
+            break;
+    }
+});
+
+API.onUpdate.connect(function () {
+    if (menuPool != null) {
+        menuPool.ProcessMenus();
+    }
+});
