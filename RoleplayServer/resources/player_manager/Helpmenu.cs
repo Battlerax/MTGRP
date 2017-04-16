da195a0d773f7ae8921deca672302214a72d1ed2
ImToro <kiwiboytrent2@gmail.com>
2017-04-17 00:45:33 +0800

    Base Menu script added
    Currently working on bugs. Just making it visable to all.

---
+++ RoleplayServer/resources/player_manager/Helpmenu.cs
@@ -0,0 +1,111 @@
+﻿/*
+ Help menu
+ Created: 1/04/2017
+ Author: Toro
+ Todo: 
+ - Add commands when they added to the server
+ - Set up another menu within the menu for FAQ
+
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTANetworkServer;
+using GTANetworkShared;
+using MongoDB.Driver;
+using RoleplayServer.resources.database_manager;
+using RoleplayServer.resources.group_manager;
+using RoleplayServer.resources.job_manager;
+using RoleplayServer.resources.AdminSystem;
+
+namespace RoleplayServer.resources.player_manager
+{
+    public class Helpmenu : Script
+    {
+        public void HelpMenu()
+        {
+            API.onClientEventTrigger += OnClientEvent;
+        }
+        [Command("help")]
+        public static void help_cmd(Client player)
+        {
+            API.triggerClientEvent(player, "HelpMenu1");
+        }
+
+
+        public void OnClientEvent(Client player, string id, params object[] args)
+        {
+            var receiver = PlayerManager.ParseClient(id);
+            Character character = API.getEntityData(player.handle, "Character");
+            Character receiverCharacter = API.getEntityData(receiver.handle, "Character");
+
+            if (id == "clickeditem")
+            {
+                if ((string)args[0] == "Commands1")
+                {
+                    player.sendChatMessage("~h~Here is the list of commands availible to you:");
+                    player.sendChatMessage("/time, /stats");
+                }
+            }
+
+            if (id == "clickeditem")
+            {
+                if ((string)args[0] == "Police1")
+                {
+                    if (character.Group == Group.None || character.Group.CommandType != Group.CommandTypeLspd)
+                    {
+                        API.sendChatMessageToPlayer(player, "You must be in the LSPD to use this command.");
+                    }
+
+                    else
+                    {
+                        player.sendChatMessage("~h~Here is the list of commands availible to you:");
+                        player.sendChatMessage("");
+                    }
+                }
+            }
+
+            if (id == "clickeditem")
+            {
+                if ((string)args[0] == "Admin1")
+                {
+                    Account account = API.shared.getEntityData(player.handle, "Account");
+                    if (account.AdminLevel == 0)
+                    {
+                        player.sendChatMessage("~h~Here is the list of commands availible to you:");
+                        player.sendChatMessage("/spec, /agiveweapon, /goto, /gotopos, /sethealth, /setarmour, /slap, /spawnveh 'name'");
+                    }
+
+                    else
+                    {
+                        player.sendChatMessage("You're not an admin, if you'd like to apply for admin head to our forums.");
+                        player.sendChatMessage("MT-gaming.com");
+                    }
+                }
+            }
+
+            if (id == "clickeditem")
+            {
+                if ((string)args[0] == "Rules1")
+                {
+                    player.sendChatMessage("To view all the rules head to ~h~MT-Gaming.com~h~ Here some basic server rules:");
+                    player.sendChatMessage("IC = In character || OOC = Out of character");
+                    player.sendChatMessage("MG = Metagaming - Using out of character info in character | Using in character info out of character.");
+                    player.sendChatMessage("DM = Death matching - killing another player without any roleplay reason or any roleplay at all.");
+                    player.sendChatMessage("PG = Powergaming - Roleplaying the impossible | forcing a player to roleplay situation without giving them a chance");
+                }
+            }
+
+            if (id == "clickeditem")
+            {
+                if ((string)args[0] == "FAQ")//Need to set another menu to pop up once this is selected to see more
+                {
+                    player.sendChatMessage("~h~This still needs to be set up - Toro");
+                }
+            }
+        }
+    }
+}
---
+++ RoleplayServer/resources/player_manager/Helpmenu.js
@@ -0,0 +1,58 @@
+﻿/*let menuPool = null;
+
+API.onUpdate.connect(function () {
+    if (menuPool != null) {
+        menuPool.ProcessMenus();
+    }
+});
+
+API.onChatCommand.connect(function (msg) {
+    if (msg == "/help") {
+        if (menupool == null) {
+            menu_pool = API.getMenuPool();
+            let menu = API.createMenu("help", " ", 0, 0, 6);
+            let item1 = API.createMenuItem("Commands", "");
+            let item2 = API.createMenuItem("Police", "");
+            let item3 = API.createMenuItem("Admin", "");
+            let item4 = API.createMenuItem("Rules", "");
+            let item5 = API.createMenuItem("FAQ", "");
+            menu.AddItem(item1);
+            menu.AddItem(item2);
+            menu.AddItem(item3);
+            menu.AddItem(item4);
+            menu.AddItem(item5);
+            menuPool.Add(menu);
+            menu.Visible = true;
+        }
+    }
+});
+*/
+//**************************************
+//*************[Example]****************
+//**************************************
+
+var menu = new Menu();
+
+function openHelpMenu() {
+    menu
+        .createMenu("Help Menu")
+        .addMenuItem("Commands", "", true, true, "callServerTrigger", "Commands1")
+        .addMenuItem("Animations", "", true, true, "callServerTrigger", "Animations1")
+        .addMenuItem("Police", "", true, true, "callServerTrigger", "Police1")
+        .addMenuItem("Admin", "", true, true, "callServerTrigger", "Admin1")
+        .addMenuItem("Rules", "", true, true, "callServerTrigger", "Rules1")
+        .addMenuItem("FAQ", "", true, true, "callServerTrigger", "FAQ1")
+        .addCloseButton()
+    ;
+}
+
+function callServerTrigger(HelpMenu1) {
+    var args = [].slice.call(arguments).splice(1);
+    menu.callServerFunction(HelpMenu1, args);
+}
+
+API.onUpdate.connect(function () {
+    if (menu.menuPool !== null) {
+        menu.menuPool.ProcessMenus();
+    }
+});
