07bb1c4bcd0448628eef22d6383c294a9bd27054
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2017-04-22 01:52:00 +0200

    Added base bag system.

---
+++ RoleplayServer/resources/inventory/bags/BagItem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTANetworkServer;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using RoleplayServer.resources.player_manager;
+
+namespace RoleplayServer.resources.inventory.bags
+{
+    class BagItem : IInventoryItem
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public int Amount { get; set; }
+
+        public int AmountOfSlots => 0; //Will return 0 cause it shouldn't be taking any slots but still have items inside.
+
+        public bool CanBeDropped => true;
+        public bool CanBeGiven => true;
+        public bool CanBeStacked => false;
+        public bool CanBeStashed => true;
+        public bool IsBlocking => false;
+        public int MaxAmount => 1;
+
+        public string CommandFriendlyName
+        {
+            get
+            {
+                //TO
+                throw new NotImplementedException();
+            }
+        }
+
+        public string LongName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Object
+        {
+            get
+            {
+                //TODO: return 
+                throw new NotImplementedException();
+            }
+        }
+
+        //-------------------------
+
+        public List<IInventoryItem> BagItems;
+
+    }
+}
---
+++ RoleplayServer/resources/inventory/bags/BagManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTANetworkServer;
+using RoleplayServer.resources.player_manager;
+
+namespace RoleplayServer.resources.inventory.bags
+{
+    class BagManager : Script
+    {
+        public BagManager()
+        {
+            API.onClientEventTrigger += API_onClientEventTrigger;
+        }
+
+        private void API_onClientEventTrigger(Client sender, string eventName, params object[] arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Command("managebag")]
+        public void Managebag(Client player)
+        {
+            Character character = API.getEntityData(player, "Character");
+            var bag = InventoryManager.DoesInventoryHaveItem<BagItem>(character);
+            if (bag == null)
+            {
+                API.sendNotificationToPlayer(player, "You don't have a bag right now.");
+                return;
+            }
+
+            //Get the current bag items.
+            string[][] bagItems = bag.BagItems.Select(x => new [] {x.Id.ToString(), x.LongName}).ToArray();
+            string[][] invItems = character.Inventory.Select(x => new[] { x.Id.ToString(), x.LongName }).ToArray();
+            API.triggerClientEvent(player, "bag_showmanager", API.toJson(invItems), API.toJson(bagItems));
+        }
+
+        //TODO: test cmd.
+        [Command("givemebag")]
+        public void GiveMeBag(Client player)
+        {
+            Character character = API.getEntityData(player, "Character");
+            API.sendChatMessageToPlayer(player, InventoryManager.GiveInventoryItem(character, new BagItem()).ToString());
+        }
+    }
+}
