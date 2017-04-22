8ed3a9c00ca71c81ed8c5b9df122722d68b12a17
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2017-04-22 14:05:32 +0200

    Removed bag system gui and some of /putinbag.

--- RoleplayServer/resources/inventory/bags/BagManager.cs
+++ RoleplayServer/resources/inventory/bags/BagManager.cs
@@ -21,10 +21,10 @@ namespace RoleplayServer.resources.inventory.bags
         }
 
         [Command("putinbag")]
-        public void putinbag(Client player, string itemname, int amount)
+        public void PutInBag(Client player, string itemname, int amount)
         {
             Character character = API.getEntityData(player, "Character");
-            var bag = InventoryManager.DoesInventoryHaveItem<BagItem>(character);
+            var bag = InventoryManager.DoesInventoryHaveItem(character, typeof(BagItem));
             if (bag == null)
             {
                 API.sendNotificationToPlayer(player, "You don't have a bag right now.");
@@ -33,7 +33,28 @@ namespace RoleplayServer.resources.inventory.bags
 
             //Parse for that item.
             Type itemType = InventoryManager.ParseInventoryItem(itemname);
+            if (itemType == null)
+            {
+                API.sendNotificationToPlayer(player, "That item doesn't exist.");
+                return;
+            }
             var item = InventoryManager.DoesInventoryHaveItem(character, itemType);
+            if (item == null)
+            {
+                API.sendNotificationToPlayer(player, "You don't have that item.");
+                return;
+            }
+
+            //Check of has enough amount.
+            if (item.Amount >= amount)
+            {
+                //Add to bag first.
+
+            }
+            else
+            {
+                API.sendNotificationToPlayer(player, "You don't have that amount.");
+            }
         }
 
         //TODO: test cmd.
