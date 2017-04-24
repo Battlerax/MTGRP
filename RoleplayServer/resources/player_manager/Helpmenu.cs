b04b31413263eb1e89e50e0861f9c57974a75f04
ImToro <kiwiboytrent2@gmail.com>
2017-04-24 22:04:44 +0800

    Working /help - Still needs FAQ menu
    Have it all workign except for the FAQ part with no menu showing up at
    the moment. Will work on a fix later.

--- RoleplayServer/resources/player_manager/Helpmenu.cs
+++ RoleplayServer/resources/player_manager/Helpmenu.cs
@@ -31,92 +31,11 @@ namespace RoleplayServer.resources.player_manager
         }
         public void onClientEvent(Client player, string id, params object[] arguments)
         {
-
         }
         [Command("help")]
         public void Helpmenu(Client player)
         {
             API.triggerClientEvent(player, "openHelpMenu", "Help_Menu", "Select an option");
         }
-        [Command("ping")]
-        public void test(Client player)
-
-        {
-            API.sendChatMessageToPlayer(player, "pong");
-        }
     } 
 }
-        /*
-        public void OnClientEvent(Client player, string id)
-        {
-            var receiver = PlayerManager.ParseClient(id);
-            Character character = API.getEntityData(player.handle, "Character");
-
-            if (id == "clickeditem")
-            {
-                if ((string)args[0] == "Commands1")
-                {
-                    player.sendChatMessage("~h~Here is the list of commands availible to you:");
-                    player.sendChatMessage("/time, /stats");
-                }
-            }
-
-            if (id == "clickeditem")
-            {
-                if ((string)args[0] == "Police1")
-                {
-                    if (character.Group == Group.None || character.Group.CommandType != Group.CommandTypeLspd)
-                    {
-                        API.sendChatMessageToPlayer(player, "You must be in the LSPD to use this command.");
-                    }
-
-                    else
-                    {
-                        player.sendChatMessage("~h~Here is the list of commands availible to you:");
-                        player.sendChatMessage("");
-                    }
-                }
-            }
-
-            if (id == "clickeditem")
-            {
-                if ((string)args[0] == "Admin1")
-                {
-                    Account account = API.shared.getEntityData(player.handle, "Account");
-                    if (account.AdminLevel == 0)
-                    {
-                        player.sendChatMessage("~h~Here is the list of commands availible to you:");
-                        player.sendChatMessage("/spec, /agiveweapon, /goto, /gotopos, /sethealth, /setarmour, /slap, /spawnveh 'name'");
-                    }
-
-                    else
-                    {
-                        player.sendChatMessage("You're not an admin, if you'd like to apply for admin head to our forums.");
-                        player.sendChatMessage("MT-gaming.com");
-                    }
-                }
-            }
-
-            if (id == "clickeditem")
-            {
-                if ((string)args[0] == "Rules1")
-                {
-                    player.sendChatMessage("To view all the rules head to ~h~MT-Gaming.com~h~ Here some basic server rules:");
-                    player.sendChatMessage("IC = In character || OOC = Out of character");
-                    player.sendChatMessage("MG = Metagaming - Using out of character info in character | Using in character info out of character.");
-                    player.sendChatMessage("DM = Death matching - killing another player without any roleplay reason or any roleplay at all.");
-                    player.sendChatMessage("PG = Powergaming - Roleplaying the impossible | forcing a player to roleplay situation without giving them a chance");
-                }
-            }
-
-            if (id == "clickeditem")
-            {
-                if ((string)args[0] == "FAQ")//Need to set another menu to pop up once this is selected to see more
-                {
-                    player.sendChatMessage("~h~This still needs to be set up - Toro");
-                }
-            }
-        }
-    }
-}
-*/
--- RoleplayServer/resources/player_manager/Helpmenu.js
+++ RoleplayServer/resources/player_manager/Helpmenu.js
@@ -4,26 +4,87 @@ API.onServerEventTrigger.connect(function (openHelpMenu, args) {
         case "openHelpMenu":
             menu
                 .createMenu("Help Menu")
-                .addMenuItem("Commands", "", true, true, "showHelpCommands", "Commands")
-                .addMenuItem("Animations", "", true, true, "callServerTrigger", "Animations")
-                .addMenuItem("Police", "", true, true, "callServerTrigger", "Police")
-                .addMenuItem("Admin", "", true, true, "callServerTrigger", "Admin")
-                .addMenuItem("Rules", "", true, true, "callServerTrigger", "Rules")
-                .addMenuItem("FAQ", "", true, true, "callServerTrigger", "FAQ")
+                .addMenuItem("Commands", "", true, true, "showHelpCmds")
+                .addMenuItem("Animations", "", true, true, "showAnimationsCmds")
+                .addMenuItem("Police", "", true, true, "showPoliceCmds")
+                .addMenuItem("Admin", "", true, true, "showAdminCmds")
+                .addMenuItem("Rules", "", true, true, "showRules")
+                .addMenuItem("FAQ", "", true, true, "showFAQ")
             break;
     }
 });
 
-function showHelpCommands(group) {
-    switch (group) {
-        case "Commands":
-            API.sendChatMessage('~h~Here is the list of commands availible to you:');
-            API.sendChatMessage('/time, /stats');
-            break;
-    }
+function showHelpCmds(group) {
+    API.sendChatMessage('~h~Here is the list of commands availible to you:');
+    API.sendChatMessage('/Time, /Stats, /Rp, /B, /Me, /Ame, /(V)ip, /(N)ewbie, /O, /Pm');
 };
-
-
+function showAnimationsCmds(group) {
+    API.sendChatMessage('~h~Here is the list of Animations:');
+    API.sendChatMessage('/Stopanim, /Hide, /Lookout, /Crowdcontrol, /Investigate, /Drink, /Crossarms, /Idle, /Lean,');
+    API.sendChatMessage('/Reach, /Workout, /Smoke, /Binoculars, /Hobo, /Fallover, /Laydown, /Drunk, /Twitchy, /Signal,');
+    API.sendChatMessage('/Cheer, /Drugdeal, /Gardening, /Guard, /Jog, /Getjiggy, /Sit, /Mech, /Yoga, /Bonghit,');
+    API.sendChatMessage('/Restrained, /MiddleFinger, /Salute, /Slowclap, /Facepalm, /Handsup, /CoverL, /CoverR');
+    API.sendChatMessage('~h~~y~VIP Animations');
+    API.sendChatMessage('/Clipboard, /Hammer, /Guitar  ');
+};
+function showPoliceCmds(group) {
+    API.sendChatMessage("~h~Here is the list of commands availible to you:");
+    API.sendChatMessage("/Arrest, /Frisk, /Cuff, /Uncuff, /Detain, /(M)egaphone");
+};
+function showAdminCmds(group) {
+    API.sendChatMessage('~h~Here is the list of Admin Commands:');
+    API.sendChatMessage('/Agiveweapon, /Spec, /Specoff, /Setadmin, /Spawnveh, /Sethealth, /Setarmour, /Gotopos');
+    API.sendChatMessage('~r~TesterCMDs to be removed.');
+    API.sendChatMessage('~r~/testeradmin');
+};
+function showRules(group) {
+    API.sendChatMessage("To view all the rules head to ~h~MT-Gaming.com~h~ Here some basic server rules:");
+    API.sendChatMessage("IC = In character || OOC = Out of character");
+    API.sendChatMessage("MG = Metagaming - Using out of character info in character | Using in character info out of character.");
+    API.sendChatMessage("DM = Death matching - killing another player without any roleplay reason or any roleplay at all.");
+    API.sendChatMessage("PG = Powergaming - Roleplaying the impossible | forcing a player to roleplay situation without giving them a chance.");
+};
+function showFAQ() {
+    menu
+        .createMenu("FAQ")
+        .addMenuItem("How to join LSPD?", "", true, true, "JoinLSPD")
+        .addMenuItem("Where is the bank?", "", true, true, "FBank")
+        .addMenuItem("Where do I find X job?", "", true, true, "FJob")
+        .addMenuItem("Where do I find X busienss?", "", true, true, "FBusienss")
+        .addMenuItem("How do I change my clothes?", "", true, true, "ChangeClothes")
+        .addMenuItem("Where do I buy a car?", "", true, true, "BuyCar")
+        .addMenuItem("Whats the best job?", "", true, true, "BestJob")
+        .addMenuItem("How do I buy ~y~VIP", "", true, true, "BuyVIP")
+};
+function JoinLSPD(group) {
+    API.sendChatMessage('Head over to ~h~Mt-Gaming.com~h~ and go to the GTA:V LSPD section to see the requirements');
+    API.sendChatMessage('General OOC requirements: Minimum 25hours, No active warnings, Able to use TeamSpeak.');
+    API.sendChatMessage('General IC requirements: Must be 21 years if age, Valid Drivers licence, No criminal record.');
+};
+function FBank(group) {
+    API.sendChatMessage('~r~Currently no /map, ask through /n for help.');
+};
+function FJob(group) {
+    API.sendChatMessage('The icons on the map indicate the location of each job.');
+};
+function FBusienss(group) {
+    API.sendChatMessage('~r~Currently no /map, ask through /n for help.');
+};
+function ChangeClothes(group) {
+    API.sendChatMessage('Head to a clothes store to purchause new clothes.');
+};
+function BuyCar(group) {
+    API.sendChatMessage('Head to the dealership on the map to purchause a vehicle inside. Or you can RP with others');
+    API.sendChatMessage('to purchause a vehicle off them second hand. (Requires 2 hours playtime)');
+};
+function BestJob(group) {
+    API.sendChatMessage('All jobs are made to be very similar with how much money you can earn. Even RPing with others');
+    API.sendChatMessage('can be a great way to earn money. We suggest you try them all to see what suits your RP and');
+    API.sendChatMessage('is enjoyable to you.');
+};
+function BuyVIP(group) {
+    API.sendChatMessage('Head over to ~h~Mt-Gaming.com~h~ Once logged in, click on the ~h~Account upgrades~h~ tab.');
+};
 
 function callServerTrigger(Helpmenu) {
     var args = [].slice.call(arguments).splice(1);
