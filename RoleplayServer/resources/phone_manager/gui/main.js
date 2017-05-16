c178533aa4d93fecc381da51efcdac51c3ccf86e
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2017-05-17 00:44:12 +0200

    Added the base gui.

---
+++ RoleplayServer/resources/phone_manager/gui/main.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $(".tile").click(function () {
+        var file = $(this).data("file");
+       
+        //Load app: 
+        $("#app-content").load("apps/" + file);
+        $("#apps-list").css("display", "none"); //Set the apps list as hidden.
+        $("#app-content").css("display", "block"); //Show the div.
+    });
+});
