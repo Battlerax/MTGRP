26adcb9fd336df21875f113cde9a1cafb9a9efe8
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2017-05-17 19:34:50 +0200

    Made home button to work.

--- RoleplayServer/resources/phone_manager/gui/main.js
+++ RoleplayServer/resources/phone_manager/gui/main.js
@@ -5,6 +5,13 @@
         //Load app: 
         $("#app-browser").attr("src", "apps/" + file);
         $("#apps-list").css("display", "none"); //Set the apps list as hidden.
-        $("#app-content").css("display", "block"); //Show the div.
+        $("#app-content").css("display", "block"); //Show the content.
+    });
+
+    $("#navigation").click(function () {
+        //Unload app: 
+        $("#app-browser").attr("src", "");
+        $("#apps-list").css("display", "block"); //Set the apps list as shown.
+        $("#app-content").css("display", "none"); //Hide the content.
     });
 });
