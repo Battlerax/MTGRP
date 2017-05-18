378b508fd8470f179d000441268535d3b7227183
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2017-05-18 23:11:16 +0200

    Added incoming call and calling styles.

--- RoleplayServer/resources/phone_manager/gui/main.js
+++ RoleplayServer/resources/phone_manager/gui/main.js
@@ -13,5 +13,34 @@
         $("#app-browser").attr("src", "");
         $("#apps-list").css("display", "block"); //Set the apps list as shown.
         $("#app-content").css("display", "none"); //Hide the content.
+        //TODO: End current call.
+        $("#call-interface").css("display", "none");
     });
 });
+
+function incoming_call(name, number)
+{
+	$("#apps-list").css("display", "none");
+    $("#app-content").css("display", "none"); 
+    $("#call-interface").css("display", "block");
+    $("#calling_header").text("INCOMING CALL");
+	$("#caller_name").text(name);	
+	$("#caller_number").text(number);
+	$("#calling_end-call").css("display", "none");
+	$("#calling_answer").css("display", "inline");
+	$("#calling_ignore").css("display", "inline");
+	$("#calling_text-reply").css("display", "inline");
+}
+
+function calling(name, number) {
+    $("#apps-list").css("display", "none");
+    $("#app-content").css("display", "none");
+    $("#call-interface").css("display", "block");
+    $("#calling_header").text("CALLING");
+    $("#caller_name").text(name);
+    $("#caller_number").text(number);
+    $("#calling_end-call").css("display", "inline");
+    $("#calling_answer").css("display", "none");
+    $("#calling_ignore").css("display", "none");
+    $("#calling_text-reply").css("display", "none");
+}
