a679789ffe1ede45d98840c864a7ee97f12df95f
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2017-05-18 02:16:11 +0200

    Added to show/hide mouse by using M button.

--- RoleplayServer/resources/phone_manager/PhoneManager.js
+++ RoleplayServer/resources/phone_manager/PhoneManager.js
@@ -3,6 +3,11 @@
 API.onServerEventTrigger.connect((eventName, args) => {
     switch (eventName) {
         case "phone_showphone":
+            if (myBrowser == null) {
+                API.sendChatMessage("You already have the phone opened.");
+                break;
+            }
+
             var res = API.getScreenResolution();
             var width = 405;
             var height = 590;
@@ -11,18 +16,18 @@ API.onServerEventTrigger.connect((eventName, args) => {
             API.setCefBrowserPosition(myBrowser, res.Width - width,
                 res.Height - height);
             API.loadPageCefBrowser(myBrowser, "phone_manager/gui/main.html");
-            API.setCanOpenChat(false);
-            API.showCursor(true);
             break;
     }
 });
 
-
+var isMouseShown = false;
 API.onKeyUp.connect(function (sender, e) {
     if (myBrowser !== null && e.KeyCode === Keys.Escape) {
         API.destroyCefBrowser(myBrowser);
-        API.showCursor(false);
-        API.setCanOpenChat(true);
         myBrowser = null;
+    }
+    else if (myBrowser !== null && e.KeyCode === Keys.M) {
+        isMouseShown = !isMouseShown;
+        API.showCursor(isMouseShown);
     }
 })
