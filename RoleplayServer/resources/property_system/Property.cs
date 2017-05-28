e0699f763a7ae3d4ac90273826963f5da076b7c2
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2017-05-28 06:37:31 +0200

    Begining Business.

---
+++ RoleplayServer/resources/property_system/Property.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace RoleplayServer.resources.property_system
+{
+    class Property
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public int OwnerId { get; set; }
+        public string PropertyName { get; set; }
+        public PropertyManager.PropertyTypes Type { get; set; }
+    }
+}
---
+++ RoleplayServer/resources/property_system/PropertyManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoleplayServer.resources.property_system
+{
+    class PropertyManager
+    {
+        public enum PropertyTypes
+        {
+            Clothing,
+            TwentyFourSeven,
+            Hardware,
+            Restaurent,
+            Bank
+        }
+    }
+}
