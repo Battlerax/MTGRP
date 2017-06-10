089d290050473c29ccd07d281ecc47cced29dcd3
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2017-06-10 22:50:25 +0300

    Some few changes into the Lsnn.js

--- RoleplayServer/resources/group_manager/lsnn/Lsnn.js
+++ RoleplayServer/resources/group_manager/lsnn/Lsnn.js
@@ -1,12 +1,14 @@
 
+var lowerthird;
+
 API.onServerEventTrigger.connect(function (eventName, args) {
     switch (eventName) {
 		
         case "watch_broadcast":
-			var cam_view = API.createCamera(args[0], args[1]);
-			API.setActiveCamera(cam_view);
-			var resolution = API.getScreenResolution();
-			lowerthird = API.createCefBrowser(resolution.Width, resolution.Height);
+			var wbcamView = API.createCamera(args[0], args[1]);
+			API.setActiveCamera(wbcamView);
+			var wbres = API.getScreenResolution();
+			lowerthird = API.createCefBrowser(wbres.Width, wbres.Height);
 			API.waitUntilCefBrowserInit(lowerthird);
 			API.setCefBrowserPosition(lowerthird, 800, 700);
 			API.loadPageCefBrowser(lowerthird, "group_manager/lsnn/LowerThird.html");
@@ -15,9 +17,9 @@ API.onServerEventTrigger.connect(function (eventName, args) {
 			break;
 
 		case "watch_chopper_broadcast":
-			var cam_view = API.createCamera(args[0], args[1]);
-			API.attachCameraToEntity(cam_view, args[3], args[4]);
-			API.setActiveCamera(cam_view);
+			var wcCamView = API.createCamera(args[0], args[1]);
+			API.attachCameraToEntity(wcCamView, args[3], args[4]);
+			API.setActiveCamera(wcCamView);
 		    API.callNative("0xBB7454BAFF08FE25", args[5], args[6], args[7], 0.0, 0.0, 0.0); 
 			var resolution = API.getScreenResolution();
 			lowerthird = API.createCefBrowser(resolution.Width, resolution.Height);
