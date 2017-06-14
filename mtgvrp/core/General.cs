9715fa3b0a59b986738768a09065a45dc1ff9f51
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2017-06-15 00:41:39 +0300

    Added GetXYBehindVehicle and Tuple library

---
+++ mtgvrp/core/General.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTANetworkServer;
+using GTANetworkShared;
+
+namespace mtgvrp.core
+{
+    class General
+    {
+
+
+        public (float, float) GetXYBehindVehicle(NetHandle vehicle, float distance)
+        {
+            var pos = API.shared.getEntityPosition(vehicle);
+            var rot = API.shared.getEntityRotation(vehicle);
+            var q = (float)(pos.X + (distance * -Math.Sin(-rot.Z)));
+            var w = (float)(pos.Y + (distance * -Math.Cos(-rot.Z)));
+            return (q, w);
+        }
+    }
+}
