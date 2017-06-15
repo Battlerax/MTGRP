49d71deb30ca552e643b8e720c85ac9f61db1287
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2017-06-15 03:52:55 +0300

    IsPlayerFacingVehicle

--- mtgvrp/core/General.cs
+++ mtgvrp/core/General.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using GTANetworkServer;
@@ -8,17 +10,32 @@ using GTANetworkShared;
 
 namespace mtgvrp.core
 {
-    class General
+    public static class General
     {
 
 
-        public (float, float) GetXYBehindVehicle(NetHandle vehicle, float distance)
+        public static bool IsPlayerFacingVehicle(NetHandle vehicle, NetHandle player, bool reverse = false)
         {
-            var pos = API.shared.getEntityPosition(vehicle);
-            var rot = API.shared.getEntityRotation(vehicle);
-            var q = (float)(pos.X + (distance * -Math.Sin(-rot.Z)));
-            var w = (float)(pos.Y + (distance * -Math.Cos(-rot.Z)));
-            return (q, w);
+            var playerpos = API.shared.getEntityPosition(player);
+            var vehiclepos = API.shared.getEntityPosition(vehicle);
+            var distance = playerpos.DistanceTo(vehiclepos);
+            if (distance < 6.0)
+            {
+                var angle = API.shared.getEntityRotation(vehicle).Z;
+
+                if (reverse)
+                    angle += 180;
+                
+                vehiclepos.X += (float)(distance * Math.Sin(-angle));
+                vehiclepos.Y += (float)(distance * Math.Cos(-angle));
+                distance = playerpos.DistanceTo(vehiclepos);
+
+                if (distance < 1.0)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
