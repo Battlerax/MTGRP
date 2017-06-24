68e17d3895d5c645953d0cb291b388c54795ea85
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2017-06-24 18:49:47 +0300

    Fixed bug with vehicle menu

--- mtgvrp/vehicle_manager/VehicleMenu.js
+++ mtgvrp/vehicle_manager/VehicleMenu.js
@@ -44,6 +44,7 @@ API.onKeyDown.connect(function(Player, args){
                     
 					if(canparkcar)
 						park_car_item = API.createMenuItem("Park Car", "Save the vehicles spawn point to its current location");
+						
                 }
                 else {
                     if (API.getVehicleEngineStatus(player_veh) == false) {
@@ -65,13 +66,14 @@ API.onKeyDown.connect(function(Player, args){
 
             door_item = API.createListItem("Door Options", "Open and close the vehicle doors.", door_list, 0);
 
-            if (player_seat == -1) {
+            if (player_seat == -1 && engine_state_item !== null) {
                 vehicle_menu.AddItem(engine_state_item);
             }
 
-            vehicle_menu.AddItem(lock_state_item);
+			if(lock_state_item !== null)
+				vehicle_menu.AddItem(lock_state_item);
 
-            if (player_seat == -1 && player_owns_veh == true) {
+            if (player_seat == -1 && player_owns_veh == true && park_car_item !== null) {
                 vehicle_menu.AddItem(park_car_item);
             }
 
