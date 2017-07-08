b090f5f4aee4e9a94a6f724dd676b8b5d6ed75f3
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2017-07-08 22:40:38 +0300

    Changed library of discord

--- mtgvrp/core/DiscordManager.cs
+++ mtgvrp/core/DiscordManager.cs
@@ -43,7 +43,7 @@ namespace mtgvrp.core
             // then we want to instantiate our client
             Client = new DiscordClient(cfg);
 
-            Client.SetWebSocketClient<WebSocketSharpClient>();
+            Client.SetWebSocketClient<WebSocket4NetClient>();
 
             // next, let's hook some events, so we know
             // what's going on
