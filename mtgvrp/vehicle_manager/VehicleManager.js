f5471862aa553f6f5b840f7296e330609d4b6665
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2017-07-22 21:31:40 +0300

    Added something to prevent vehicles gas leaking.

---
+++ mtgvrp/vehicle_manager/VehicleManager.js
@@ -0,0 +1,6 @@
+﻿API.onEntityStreamIn.connect((ent, entType) => {
+    if (entType === 1) { // Vehicle
+        API.callNative('SET_DISABLE_VEHICLE_PETROL_TANK_FIRES', ent, true);
+        API.callNative('SET_DISABLE_VEHICLE_PETROL_TANK_DAMAGE', ent, true);
+    }
+});
