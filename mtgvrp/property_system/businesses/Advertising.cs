8c1cd9f6b914a96c32688996a761804069735f0a
AlexanderNorton <alexander.norton5@mail.dcu.ie>
2017-07-29 05:27:48 +0100

    Fixed advertising - would find all players and not characters

--- mtgvrp/property_system/businesses/Advertising.cs
+++ mtgvrp/property_system/businesses/Advertising.cs
@@ -62,17 +62,16 @@ namespace mtgvrp.property_system.businesses
                 return;
             }
 
-            foreach (var receiver in API.getAllPlayers())
+            foreach (var receiver in PlayerManager.Players)
             {
-                Character receiverchar = API.getEntityData(receiver, "Character");
                 var senderPhone = InventoryManager.DoesInventoryHaveItem<Phone>(character)[0];
-                var receiverPhone = InventoryManager.DoesInventoryHaveItem<Phone>(receiverchar)[0];
-
+                var receiverPhone = InventoryManager.DoesInventoryHaveItem<Phone>(receiver)[0];
+                
                 InventoryManager.DeleteInventoryItem(character, typeof(Money), price);
 
                 if (receiverPhone.IsOn)
                 {
-                    receiver.sendChatMessage("~g~[AD] (#" + senderPhone.PhoneNumber + "): " + text);
+                    receiver.Client.sendChatMessage("~g~[AD] (#" + senderPhone.PhoneNumber + "): " + text);
                 }
 
                 player.sendChatMessage("Advertisement subimtted.");
