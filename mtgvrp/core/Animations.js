bc12ecf5ede36281b950bc65f3254aea952c535b
devkroma <joejames1996@hotmail.com>
2017-08-12 14:06:32 +0100

    Checking if player is underwater whilst in animation

--- mtgvrp/core/Animations.js
+++ mtgvrp/core/Animations.js
@@ -16,3 +16,12 @@ API.onServerEventTrigger.connect(function (eventName, args) {
             break;
     }
 });
+
+API.onUpdate.connect(function () {
+    if(isInAnimation) {
+        if(API.callNative("0xCFB0A0D8EDD145A3", API.getLocalPlayer()) == true) { /* checking if player is in water */
+            isInAnimation = false;
+            API.triggerServerEvent("stopPlayerAnims");
+        }
+    }
+});
