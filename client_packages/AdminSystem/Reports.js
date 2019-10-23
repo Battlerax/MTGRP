623727b84720a1d4c534becf55dd333aacabbb37
Ahmed Mamdouh <ahmad.gasser@gmail.com>
2019-10-23 13:38:56 +0200

    Fixed Assembly duplicate problem.

--- client_packages/AdminSystem/Reports.js
+++ client_packages/AdminSystem/Reports.js
@@ -1,59 +1,56 @@
-var report_menu = null;
-var menu_pool = null;
+const NativeUI = require("nativeui");
+const Menu = NativeUI.Menu;
+const UIMenuItem = NativeUI.UIMenuItem;
+const UIMenuListItem = NativeUI.UIMenuListItem;
+const UIMenuCheckboxItem = NativeUI.UIMenuCheckboxItem;
+const UIMenuSliderItem = NativeUI.UIMenuSliderItem;
+const BadgeStyle = NativeUI.BadgeStyle;
+const Point = NativeUI.Point;
+const ItemsCollection = NativeUI.ItemsCollection;
+const Color = NativeUI.Color;
+const ListItem = NativeUI.ListItem;
+
+var report_menu = new Menu("Report Menu", "Request assistance or report a player.", new Point(50, 50));
+report_menu.AddItem(new UIMenuItem("Request Assistance", "Request administrator assistance."));
+report_menu.AddItem(new UIMenuItem("Report a Player", "Report a player for breaking the server rules."));
+report_menu.AddItem(new UIMenuItem("Close", "Close"));
+
 Event.OnServerEventTrigger.connect(function (eventName, args) {
     switch (eventName) {
         case "show_report_menu":
             {
-                menu_pool = API.getMenuPool();
-                report_menu = API.createMenu("Report Menu", "Request assistance or report a player.", 0, 0, 3);
-                report_menu.AddItem(API.createMenuItem("Request Assistance", "Request administrator assistance."));
-                report_menu.AddItem(API.createMenuItem("Report a Player", "Report a player for breaking the server rules."));
-                report_menu.AddItem(API.createMenuItem("Close", "Close"));
-                report_menu.Visible = true;
-                menu_pool.Add(report_menu);
-                report_menu.OnItemSelect.connect(function (sender, selectedItem, index) {
-                    if (selectedItem.Text == "Close") {
-						report_menu.Visible = false;
-                        report_menu = null;
-                        menu_pool = null;
+                report_menu.Open();
+                report_menu.ItemSelect.on(item => {
+                    if (item.Text == "Close") {
+						report_menu.Close();
                     }
-                    else if (selectedItem.Text == "Request Assistance"){
-					  report_menu.Visible = false;
-                      report_menu = null;
-                      menu_pool = null;
-					  API.sendNotification("~r~Enter your reason for request.");
-					  API.SendChatMessage("~r~Enter your reason for request.");
-                      var message = API.getUserInput("", 200);
-					  if(message.length == 0){
-						API.sendNotification("~r~Please enter a valid reason for your request.");
-						API.SendChatMessage("~r~Please enter a valid reason for your request.");
-						return;
-					  }
-                      API.triggerServerEvent("OnRequestSubmitted", message);
+                    else if (item.Text == "Request Assistance"){
+                        report_menu.Close();
+                        API.sendNotification("~r~Enter your reason for request.");
+                        var message = API.getUserInput("", 200);
+                        if(message.length == 0){
+                            API.sendNotification("~r~Please enter a valid reason for your request.");
+                            return;
+                        }
+                        API.triggerServerEvent("OnRequestSubmitted", message);
                     }
 
-                    else if (selectedItem.Text == "Report a Player"){
-					  report_menu.Visible = false;
-                      report_menu = null;
-                      menu_pool = null;
-					  API.sendNotification("~r~Enter the name or ID of the player you want to report.");
-					  API.SendChatMessage("~r~Enter the name or ID of the player you want to report.");
-                      var targetPlayer = API.getUserInput("",65);
-					  if(targetPlayer.length == 0){
-						API.sendNotification("~r~Please enter a valid name or ID of the player you want to report.");
-						API.SendChatMessage("~r~Please enter a valid name or ID of the player you want to report.");
-						return;
-					  }
+                    else if (item.Text == "Report a Player"){
+                        report_menu.Close();
+                        API.sendNotification("~r~Enter the name or ID of the player you want to report.");
+                        var targetPlayer = API.getUserInput("",65);
+                        if(targetPlayer.length == 0){
+                            API.sendNotification("~r~Please enter a valid name or ID of the player you want to report.");
+                            return;
+					    }
 
-					  API.sendNotification("~r~Enter your report reason.");
-					  API.SendChatMessage("~r~Enter your report reason.");
-                      var nmessage = API.getUserInput("", 200);
-                      if (nmessage.length == 0){
-						API.sendNotification("~r~Please enter a valid report reason.");
-						API.SendChatMessage("~r~Please enter a valid report reason.");
-						return;
-					  }
-                      API.triggerServerEvent("OnReportMade", nmessage, targetPlayer);
+                        API.sendNotification("~r~Enter your report reason.");
+                        var nmessage = API.getUserInput("", 200);
+                        if (nmessage.length == 0){
+                            API.sendNotification("~r~Please enter a valid report reason.");
+                            return;
+                        }
+                        API.triggerServerEvent("OnReportMade", nmessage, targetPlayer);
                     }
 
                 });
@@ -66,7 +63,7 @@ Event.OnServerEventTrigger.connect(function (eventName, args) {
         break;
         
     case "GET_CP_TO_SEND":
-API.triggerServerEvent("SET_PLAYER_CP", args[0], API.getWaypointPosition());
+        API.triggerServerEvent("SET_PLAYER_CP", args[0], API.getWaypointPosition());
     break;
   }
 });
